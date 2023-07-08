465f7defc798b51906dbbba34bc99b82e133a725
farishta18 <farishtashinwari01@gmail.com>
2023-07-08 12:33:54 -0400

    The basic Solar System is ready to be depolyed

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+       SolarSystemBasic Omnax=new SolarSystemBasic();
+       Omnax.Moon="2";
+       Omnax.Sun="2";
+       Omnax.Planets="2";
+       Omnax.Star="2";
+    }
+}
