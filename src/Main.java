d56f43ea9852edfeeb23cc9fdd4915e6f962d8a9
farishta18 <farishtashinwari01@gmail.com>
2023-07-09 10:55:52 -0400

    I have added Planet2 and it is working fine

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,12 @@ public class Main {
        AdamX.name="poya";
        AdamX.temp="12344231 celcius";
        AdamX.size="32942km";
+
+       Planet2 Feon=new Planet2();
+       Feon.name="exo";
+       Feon.color="white";
+       Feon.size="15 celicous";
+       Feon.temp="i234Billions km";
+
     }
 }
